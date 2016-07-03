8de42eefcec5d9d56aa2d12b36fa205f6441a543
Ian Minoso <iminoso@gmail.com>
2016-07-03 01:50:40 -0400

    Harry Potter Project

---
+++ names.py
@@ -0,0 +1,23 @@
+import nltk
+import os
+import json
+
+character_counter = dict()
+
+output_file = open('outputs/names_count.txt', 'w+')
+# books = list(1, 2, 3, 4, 5, 6, 7)
+books = [2]
+for i in books:
+    print 'Book {}'.format(i)
+    book_name = "book_%s.txt" % (str(i))
+    book = open(os.path.join('dataset', "book_%s.txt" % (str(i)))).read()
+    for sentences in nltk.sent_tokenize(book.decode("utf-8")):
+        for chunked in nltk.ne_chunk(nltk.pos_tag(nltk.word_tokenize(sentences))):
+            print chunked, chunked[0], type(chunked), len(chunked)
+            if type(chunked) is not tuple:
+                name = ' '.join(leaf[0] for leaf in chunked.leaves())
+                if name in character_counter:
+                    character_counter[name] += 1
+                else:
+                    character_counter[name] = 1
+print >>output_file, json.dumps(character_counter, sort_keys=True, indent=2)
