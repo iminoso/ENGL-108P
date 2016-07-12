aae677fd4135d246a4376046b3bcb4965e0d094f
Ian Minoso <iminoso@gmail.com>
2016-07-12 04:04:22 -0400

    add dispersion plots

--- names.py
+++ names.py
@@ -4,8 +4,8 @@ import json
 
 character_counter = dict()
 
-books = list(1, 2, 3, 4, 5, 6, 7)
-
+# books = list([1, 2, 3, 4, 5, 6, 7])
+books = [1]
 for i in books:
     print 'Book {}'.format(i)
     output_file = open('outputs/names_count_book_' + str(i) + '.txt', 'w+')
