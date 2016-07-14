1c4c7bdca88207b3c0bbfd80c15c7738c8e6c4e2
Ian Minoso <iminoso@gmail.com>
2016-07-13 20:10:39 -0400

    dispersion with titles

--- dispersion.py
+++ dispersion.py
@@ -5,7 +5,7 @@ import json
 character_counter = dict()
 
 # books = list([1, 2, 3, 4, 5, 6, 7])
-books = [6]
+books = [1]
 for i in books:
     print 'Book {}'.format(i)
     book_name = "book_%s.txt" % (str(i))
@@ -13,4 +13,9 @@ for i in books:
     # book.dispersion_plot(["Harry", "Voldemort"])
     tokens = nltk.word_tokenize(book.decode('utf-8'))
     mytext = nltk.Text(tokens)
-    mytext.dispersion_plot(["Harry", "Ron", "Hermione", "Malfoy", "Dumbledore", "Voldemort", "Sirius", "Hagrid", "Snape"])
+    words = ["Harry", "Ron", "Hermione", "Malfoy",
+             "Dumbledore", "Voldemort", "Sirius", "Hagrid", "Snape"]
+    nltk.draw.dispersion.dispersion_plot(
+        mytext, words, ignore_case=False, title='Word Dispersion in Philosoper\'s Stone')
+    # mytext.dispersion_plot(["Harry", "Ron", "Hermione", "Malfoy",
+                            # "Dumbledore", "Voldemort", "Sirius", "Hagrid", "Snape"])
--- ngram.py
+++ ngram.py
@@ -2,19 +2,28 @@ import nltk
 import os
 from nltk.util import ngrams
 
+
 character_counter = dict()
 
 # books = list([1, 2, 3, 4, 5, 6, 7])
+count = 0
 books = [1]
 for i in books:
     print 'Book {}'.format(i)
     book_name = "book_%s.txt" % (str(i))
     book = open(os.path.join('dataset', "book_%s.txt" % (str(i)))).read()
-    # book.dispersion_plot(["Harry", "Voldemort"])
+
     tokens = nltk.word_tokenize(book.decode('utf-8', 'ignore'))
-    # tokens = nltk.word_tokenize(book.decode('utf-8'))
-    str_bigrams = ngrams(tokens, 3)
-    for gram in str_bigrams:
-        print str(gram[0].encode('utf-8'))
-        # if gram[0].lower() == 'ron' and gram[0].lower() == 'and' and gram[2].lower() == 'hermione':
-        #     print '<<<<<<<<<<<<<<<'
+
+    for t in tokens:
+        print t
+
+    # str_bigrams = ngrams(tokens, 3)
+    # ctr = 0
+    # for gram in str_bigrams:
+    #     ctr += 1
+    #     g = [str(x.encode('utf-8')) for x in gram]
+    #     if "Ron" in g and "Hermione" in g:
+    #         print g, gram, ctr
+    #         count += 1
+print count
